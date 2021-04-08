10f8468d04e4878eb88ee4c3222afb5778ed4a87
Mateusz <matyd356@gmail.com>
2021-04-08 22:23:31 +0200

    add posts fetched from db to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const [posts, setPosts] = useState<any>([]);
+  useEffect((): void => {
+    const fetchPosts = (): void => {
+      fetch('http://localhost:5000/posts/')
+        .then(response => response.json())
+        .then(data => setPosts(data.posts))
+    }
+    fetchPosts()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!posts
+          ? <p>loading</p>
+          : posts.map((post: any) => <p key={post._id}>{post.title}</p>)
+        }
       </header>
     </div>
   );
